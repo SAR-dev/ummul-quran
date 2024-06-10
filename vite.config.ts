8cc1cf6757d8763bde0ff903ca04c9899ca346c5
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-06-10 18:45:03 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    global: {}
+  },
+  envDir: "./env",
+  server: {
+    host: '0.0.0.0',
+    port: 2020,
+  },
   plugins: [tsconfigPaths(), react()],
 })
