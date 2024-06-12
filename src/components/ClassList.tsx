2223cb3d0a23180d8f5e3d2977156e6ed6750c0c
Sayed Rafi <sayedarrafi.bd@gmail.com>
2024-06-12 21:20:39 +0900

    up

---
+++ src/components/ClassList.tsx
@@ -0,0 +1,76 @@
+import { InformationCircleIcon } from '@heroicons/react/24/outline'
+import { PlusIcon } from '@heroicons/react/24/solid';
+
+const ClassList = () => {
+    return (
+        <div className="p-8 bg-base-200 card border border-base-300 flex flex-col gap-5">
+
+            <div className="flex justify-between">
+                <div className="text-xl font-semibold">You have 5 classes today</div>
+                <div className="flex gap-3">
+                    <button className="btn btn-sm bg-base-100">
+                        Schedule a class
+                        <PlusIcon className="h-4 w-4" />
+                    </button>
+                    <button className="btn btn-sm bg-base-100">
+                        See all
+                    </button>
+                </div>
+            </div>
+
+            <div className="bg-info/30 px-6 py-3 card flex-row gap-2 items-center">
+                <InformationCircleIcon className="h-5 w-5" />
+                You have a <b>60 minutes</b> class in <b>35 minutes</b> with <b>Sayed Rafi</b> of Japan
+            </div>
+
+            <div className="relative overflow-x-auto">
+                <table className="w-full text-sm text-left table-auto">
+                    <thead className="text-xs uppercase bg-base-100/50">
+                        <tr>
+                            <th scope="col" className="px-6 py-3">
+                                #
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Date
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Time Slot
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Student
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Country
+                            </th>
+                            <th scope="col" className="px-6 py-3">
+                                Actions
+                            </th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {[...Array(3)].map((_, i) => (
+                            <tr className="bg-base-100 border-b border-base-300" key={i}>
+                                <th className="px-6 py-4">{(i + 1).toString().padStart(2, "0")}</th>
+                                <th scope="row" className="px-6 py-4">
+                                    12 July, 2024
+                                </th>
+                                <th className="px-6 py-4">0800 - 0900</th>
+                                <td className="px-6 py-4">Sayed Ar Rafi</td>
+                                <td className="px-6 py-4">Japan</td>
+                                <td className="px-6 py-4 w-64">
+                                    <div className="flex gap-2">
+                                        <button className="btn btn-sm">Class Link</button>
+                                        <button className="btn btn-sm">Cancel</button>
+                                    </div>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+
+        </div>
+    )
+}
+
+export default ClassList
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
-import { InformationCircleIcon } from "@heroicons/react/24/outline"
-import { PlusIcon } from "@heroicons/react/24/solid"
+import { ArrowUpIcon, TrashIcon } from "@heroicons/react/24/outline"
+import ClassList from "components/ClassList"
 import Navbar from "components/Navbar"
 
 const Home = () => {
@@ -9,71 +9,74 @@ const Home = () => {
 
       <div className="grid grid-cols-4 w-full">
         <div className="col-span-3">
-          <div className="p-16 w-full max-w-screen-xl">
-            <div className="p-8 bg-base-200 card border border-base-300 flex flex-col gap-5">
-              <div className="flex justify-between">
-                <div className="text-xl font-semibold">You have 5 classes today</div>
-                <div className="flex gap-3">
-                  <button className="btn btn-sm bg-base-100">
-                    Schedule a class
-                    <PlusIcon className="h-4 w-4" />
+          <div className="p-16 w-full max-w-screen-xl grid grid-cols-1 gap-10">
+            <ClassList />
+            <div className="card flex-row border border-base-300 overflow-hidden">
+              
+              <div className="flex flex-col divide-y divide-base-300 border-r border-base-300">
+                {[...Array(5)].map((_, i) => (
+                  <button className="flex gap-5 justify-center items-center p-5 hover:bg-base-300" key={i}>
+                    <div className="w-16 flex-shrink-0">
+                      <img className="w-16 h-16 card" src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" alt="" />
+                    </div>
+                    <div className="w-32 flex-shrink-0">
+                      <div className="font-medium">Sayed Rafi</div>
+                      <div className="text-sm text-base-content/75">Bangladesh</div>
+                    </div>
                   </button>
-                  <button className="btn btn-sm bg-base-100">
-                    See all
-                  </button>
+                ))}
+              </div>
+
+              <div className="w-full p-5">
+                <div className="card relative overflow-x-auto border border-base-300">
+                  <table className="w-full text-sm text-left table-auto">
+                    <thead className="text-xs uppercase bg-base-200">
+                      <tr>
+                        <th scope="col" className="px-6 py-3">
+                          #
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Date
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Time
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Note
+                        </th>
+                        <th scope="col" className="px-6 py-3">
+                          Actions
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {[...Array(10)].map((_, i) => (
+                        <tr className="bg-base-100 border-b border-base-300" key={i}>
+                          <th className="px-6 py-4">{(i + 1).toString().padStart(2, "0")}</th>
+                          <th scope="row" className="px-6 py-4">
+                            12 July, 2024
+                          </th>
+                          <th className="px-6 py-4">0800 - 0900</th>
+                          <td className="px-6 py-4 max-w-64">
+                            Control the transition styles of conditionally rendered elements, including nested child transitions, using CSS classes.
+                          </td>
+                          <td className="px-6 py-4 w-28">
+                            <div className="flex gap-2">
+                              <button className="btn btn-sm btn-square btn-outline border-base-300">
+                                <ArrowUpIcon className="h-5 w-5" />
+                              </button>
+                              <button className="btn btn-sm btn-square btn-outline border-base-300">
+                                <TrashIcon className="h-5 w-5" />
+                              </button>
+                            </div>
+                          </td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
                 </div>
               </div>
 
-              <div className="bg-base-100 px-6 py-3 card flex-row gap-2 items-center">
-                <InformationCircleIcon className="h-5 w-5" />
-                You have a <b>60 minutes</b> class in <b>35 minutes</b> with <b>Sayed Rafi</b> of Japan
-              </div>
-
-              <div className="relative overflow-x-auto">
-                <table className="w-full text-sm text-left table-auto">
-                  <thead className="text-xs uppercase bg-base-100/50">
-                    <tr>
-                      <th scope="col" className="px-6 py-3">
-                        #
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Date
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Time Slot
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Student
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Country
-                      </th>
-                      <th scope="col" className="px-6 py-3">
-                        Actions
-                      </th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {[...Array(5)].map((_, i) => (
-                      <tr className="bg-base-100 border-b border-base-300" key={i}>
-                        <th className="px-6 py-4">{(i + 1).toString().padStart(2, "0")}</th>
-                        <th scope="row" className="px-6 py-4">
-                          12 July, 2024
-                        </th>
-                        <th className="px-6 py-4">0800 - 0900</th>
-                        <td className="px-6 py-4">Sayed Ar Rafi</td>
-                        <td className="px-6 py-4">Japan</td>
-                        <td className="px-6 py-4 w-64">
-                          <div className="flex gap-2">
-                            <button className="btn btn-sm">Class Link</button>
-                            <button className="btn btn-sm">Cancel</button>
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
             </div>
           </div>
         </div>
